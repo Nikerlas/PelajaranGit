fe305b3d061ac060ebe9c42d5e15da38953b5077
NicholasErnesto <nicholas.ernesto23@gmail.com>
2023-12-11 19:48:33 +0700

    pelajran git

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name= 'minggu14',
+    name= 'pelajaran_git',
     version='0.1.0',
     author='Nicholas',
     author_email='yyumireko@gmail.com',
